2cdb7094993eb44383967c9f36accf9255b18476
piercecollazo <piercecollazo@gmail.com>
2019-07-18 15:56:00 -0400

    checklist controller updated with more functionality

--- routes/users/controllers/checklistController.js
+++ routes/users/controllers/checklistController.js
@@ -4,8 +4,8 @@ const Checklist = require('../models/Checklist')
 module.exports = {
     createList: (req, res)=>{
         let checklist = new Checklist()
-
-        checklist.name = req.user._id
+        checklist.owner = req.user._id
+        checklist.name = req.user.profile.name
         if(req.user.profile.status == 'Veteran'){
             checklist.ets.date = 'Completed'
             checklist.ets.ta50 = true
@@ -24,5 +24,42 @@ module.exports = {
                 res.redirect('/')
             }
         })
+    },
+    update:(params, id)=>{
+        return new Promise((resolve, reject)=>{
+            Checklist.findOne({owner: id})
+                .then(checklist=>{
+                    if(!checklist){
+                        let errors = {}
+                        errors.message = 'Cannot find user'
+                        errors.status = 400
+
+                        reject(errors)
+                    } else {
+
+                        if(params.name)checklist.name = params.name
+                        if(params.ets.date)checklist.ets.date = params.ets.date
+                        if(params.school.name)checklist.school.name = params.school.name
+                        if(params.school.grants)checklist.school.grants = params.school.grants
+                        if(params.school.benefits)checklist.school.benefits = params.school.benefits
+                        if(params.school.attendance)checklist.school.attendance = params.school.attendance
+                        if(params.housing)checklist.housing = params.housing
+                        if(params.employment)checklist.employment = params.employment
+                        
+                        checklist.save((error) => {
+                            if (error) {
+                                res.status(400).json({
+                                    confirmation: 'failure',
+                                    message: error
+                                })
+                            } else {
+                                console.log('Checklist update successfully saved!')
+                                res.redirect('/')
+                            }
+                        })
+                    }
+        })
+        .catch(error => reject(error))
+    })
     }
 }
